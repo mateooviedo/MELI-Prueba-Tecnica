1fc614c3cd0eb65379846f054831ce6520acc4ff
mateooviedo <mateooviedotorres@gmail.com>
2022-05-27 01:23:17 -0500

    Enable cors for all

--- meli-api/index.js
+++ meli-api/index.js
@@ -2,20 +2,8 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 
-const whitelist = ["http://localhost:3000"]
-const corsOptions = {
-  origin: function (origin, callback) {
-    if (!origin || whitelist.indexOf(origin) !== -1) {
-      callback(null, true)
-    } else {
-      callback(new Error("Not allowed by CORS"))
-    }
-  },
-  credentials: true,
-}
-
 // Enable cors
-app.use(cors(corsOptions))
+app.use(cors())
 
 // Set port
 app.set('port', process.env.PORT || 3030);
